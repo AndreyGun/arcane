c761672503d6203cdbc4c0b0a65dc0c3570124ec
AndreyGun <andreygunkin90@gmail.com>
2022-02-01 17:01:52 +0200

    store from useTypedSelector

--- src/pages/UserDetailsPage/UserDetailsPage.tsx
+++ src/pages/UserDetailsPage/UserDetailsPage.tsx
@@ -3,15 +3,18 @@ import { useParams } from 'react-router-dom';
 import Loading from '../../component/Loading/Loading';
 import { IUser } from '../../types/usersTypes';
 import UserItemDetails from '../../component/UserItemDetails/UserItemDetails';
-import { useDispatch, useStore } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { getUserById } from '../../redux/action/getUserById';
+import { useTypedSelector } from '../../hooks/useTypedSelector';
 
 const UserDetailsPage: FC = () => {
     let params = useParams();
     const dispatch = useDispatch();
 
-    const store = useStore().getState().users;
-    const { users, loading } = store;
+    // const store = useStore().getState().users;
+    const storeUseSelector = useTypedSelector( store => store.users );
+    
+    const { users, loading } = storeUseSelector;
     const user: IUser | null = users.length ? users.filter((u: IUser) => u.id === Number(params.id))[0] : null;
     interface Props {
         user: IUser | null
